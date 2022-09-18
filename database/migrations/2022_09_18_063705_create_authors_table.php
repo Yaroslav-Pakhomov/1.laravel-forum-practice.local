5132fdfac0370db30745d165a83f3360525e02d9
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-09-18 20:37:16 +0500

    Миграции и модели, отладка

---
+++ database/migrations/2022_09_18_063705_create_authors_table.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    private const TABLE = 'authors';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE, static function (Blueprint $table) {
+            $table->id();
+
+            $table->string('avatar', 30);
+            $table->string('name', 30);
+            $table->string('login', 30)->unique();
+            $table->string('email', 30)->unique();
+            $table->string('password', 255);
+            $table->unsignedBigInteger('role_id')->nullable();
+
+            $table->softDeletes();
+
+            $table->timestamps();
+
+            // IDx
+            $table->index('role_id');
+
+            // FK
+            // $table->foreign('role_id')->on('roles')->references('id')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
---
+++ database/migrations/2022_09_18_070113_create_comments_table.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    private const TABLE = 'comments';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE, static function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 70);
+            $table->text('content');
+
+            $table->unsignedBigInteger('author_id')->nullable();
+            $table->unsignedBigInteger('post_id')->nullable();
+
+            $table->softDeletes();
+
+            $table->timestamps();
+
+            // IDx
+            $table->index('author_id');
+            $table->index('post_id');
+
+            //FK
+            // $table->foreign('author_id','comment_author_fk')->on('authors')->references('id')->cascadeOnUpdate()->nullOnDelete();
+            // $table->foreign('post_id','comment_post_fk')->on('posts')->references('id')->cascadeOnUpdate()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
