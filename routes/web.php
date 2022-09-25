a16e22a5056ede6a3ce5c60cfc0c3c2440f1cd31
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-09-25 22:05:30 +0500

    Маршрутизатор и контроллеры

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
+declare(strict_types=1);
 
+use App\Http\Controllers\SectionController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', static function () {
+//     return view('welcome');
+// });
+
+
+
+// ----------------------
+// Форум - начало
+// ----------------------
+
+// Раздел
+Route::controller(SectionController::class)->group(function () {
+    Route::name('section.')->group(function () {
+        // Главная с разделами
+        Route::get('/', 'index')->name('index');
+
+        // Страница раздела
+        Route::get('/{id}', 'show')->whereNumber('id')->name('show');
+    });
 });
+
+// Тема
+Route::controller(ThemeController::class)->group(function () {
+    Route::prefix('/{id}/theme')->group(function () {
+        Route::name('theme.')->group(function () {
+
+        // Страница темы
+        Route::get('/{theme_id}', 'index')->whereNumber(['id', 'theme_id'])->name('index');
+        });
+
+        // Создание темы
+        Route::get('create', 'create')->whereNumber('id')->name('create');
+        Route::post('/', 'store')->whereNumber('id')->name('store');
+
+        // Редактирование темы
+        Route::get('/{theme_id}/edit', 'edit')->whereNumber(['id', 'theme_id'])->name('edit');
+        Route::patch('/{theme_id}', 'update')->whereNumber(['id', 'theme_id'])->name('update');
+
+        // Удаление темы
+        Route::delete('/{theme_id}', 'delete')->whereNumber(['id', 'theme_id'])->name('delete');
+    });
+});
+
+
+
+
+
+// ----------------------
+// Форум - конец
+// ----------------------
