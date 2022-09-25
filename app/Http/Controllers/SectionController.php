18f6af6c3e71bcfcf3626df499a58a5d8a203789
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-09-25 22:05:30 +0500

    Маршрутизатор и контроллеры, отладка

---
+++ app/Http/Controllers/SectionController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Section;
+use Illuminate\Contracts\View\View;
+
+class SectionController extends Controller
+{
+    public function index(): View
+    {
+        $title = 'Список всех разделов';
+        $sections = Section::all();
+
+        return view('section.index', compact('title', 'sections'));
+    }
+
+    public function show($slug_section): View
+    {
+        $title = 'Страница раздела';
+        $section = Section::query()->with([ 'themes' => function ($query) {
+            // Ограничение нетерпеливой загрузки
+            $query->orderBy('updated_at', 'desc');
+        } ])->where('slug', $slug_section)->get();
+
+        return view('section.show', compact('title', 'section'));
+    }
+}
