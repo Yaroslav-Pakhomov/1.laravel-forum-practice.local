d16cb79432d0a42e28a1dbe1ccdafcd3fc73f4d2
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-10-15 11:13:13 +0500

    Модели, отладка

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -6,8 +6,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Section extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * Таблица, связанная с моделью.
+     *
+     * @var string
+     */
+    protected $table = 'sections';
+
+    /**
+     * Связь один к одному (каждый пост ИМЕЕТ свою миниатюру), у таблицы 'thumbnails' есть внешний ключ 'post_id', который ссылается на таблицу 'posts'
+     */
+    // public function thumbnail() {
+    //     return $this->hasOne(Thumbnail::class);
+    // }
+    //
+    // /**
+    //  * Обратная связь один к одному (каждая миниатюра ПРИНАДЛЕЖИТ посту), у таблицы 'thumbnails' есть внешний ключ 'post_id', который ссылается на таблицу 'posts'
+    //  */
+    // public function post() {
+    //     return $this->belongsTo(Post::class);
+    // }
+
+    /**
+     * Отношение один ко многим (один раздел ИМЕЕТ несколько тем), у таблицы 'themes' есть внешний ключ 'section_id', который ссылается на таблицу 'sections'
+     */
+    public function themes(): HasMany
+    {
+        return $this->hasMany(Theme::class);
+    }
+
 }
--- app/Models/Theme.php
+++ app/Models/Theme.php
@@ -6,8 +6,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Theme extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * Таблица, связанная с моделью.
+     *
+     * @var string
+     */
+    protected $table = 'themes';
+
+    /**
+     * Обратная связь один ко многим (каждая тема ПРИНАДЛЕЖИТ разделу), у таблицы 'themes' есть внешний ключ 'section_id', который ссылается на таблицу 'sections'
+     */
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(Section::class);
+    }
+
+    /**
+     * Обратная связь один ко многим (каждая тема ПРИНАДЛЕЖИТ разделу), у таблицы 'themes' есть внешний ключ 'section_id', который ссылается на таблицу 'sections'
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    /**
+     * Отношение один ко многим (одна тема ИМЕЕТ несколько постов), у таблицы 'posts' есть внешний ключ 'theme_id', который ссылается на таблицу 'themes'
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
